8cd932650b4679034493afeb401bd5cdd3d8229d
wadsaek <wadsaek@gmail.com>
2025-09-04 08:40:01 +0300

    add empty projects

---
+++ FileService/Program.cs
@@ -0,0 +1,19 @@
+using ZipZap.FileService.Services;
+using Microsoft.AspNetCore.Builder;
+
+namespace ZipZap.FileService;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+        app.Run();
+    }
+}
