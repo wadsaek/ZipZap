cb5d12313a20686c1df658b7d4b8e2049d9fbb4f
wadsaek <wadsaek@gmail.com>
2025-10-09 14:16:26 +0300

    make a deffered class

--- FileService/Extensions/NpgsqlConnectionExt.cs
+++ FileService/Extensions/NpgsqlConnectionExt.cs
@@ -1,32 +1,24 @@
 using System;
 using System.Threading.Tasks;
 using Npgsql;
+using ZipZap.FileService.Helpers;
 
 namespace ZipZap.FileService.Extensions;
-public static class NpgsqlConnectionExt{
-    extension(NpgsqlConnection connection){
-        public async Task<IAsyncDisposable> OpenAsyncDisposable(){
+
+public static class NpgsqlConnectionExt {
+    extension(NpgsqlConnection connection) {
+        public async Task<IAsyncDisposable> OpenAsyncDisposable() {
             await connection.OpenAsync();
-            return new DisposableOpenConnection(connection);
+            return new Deffered<NpgsqlConnection>(connection,
+                    conn => conn.Close(),
+                    conn => conn.CloseAsync()
+                    );
         }
-        public NpgsqlCommand CreateCommand(string cmd){
+
+        public NpgsqlCommand CreateCommand(string cmd) {
             var command = connection.CreateCommand();
             command.CommandText = cmd;
             return command;
-        }
-    }
-    private class DisposableOpenConnection : IAsyncDisposable, IDisposable {
-        private readonly NpgsqlConnection _connection;
-
-        public DisposableOpenConnection(NpgsqlConnection connection) {
-            _connection = connection;
-        }
-
-        public void Dispose() =>
-            _connection.Close();
-
-        public async ValueTask DisposeAsync() {
-            await _connection.CloseAsync();
         }
     }
 }
---
+++ FileService/Helpers/Deffered.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Threading.Tasks;
+
+namespace ZipZap.FileService.Helpers;
+
+public class Deffered<T> : IAsyncDisposable, IDisposable {
+    private readonly T _defferedTarget;
+    private readonly Action<T> _defferedSync;
+    private readonly Func<T, Task> _defferedAsync;
+
+    public Deffered(T defferedTarget,
+            Action<T> defferedSync,
+            Func<T, Task> defferedAsync) {
+        _defferedTarget = defferedTarget;
+        _defferedSync = defferedSync;
+        _defferedAsync = defferedAsync;
+    }
+
+    public void Dispose() =>
+        _defferedSync(_defferedTarget);
+
+    public async ValueTask DisposeAsync() =>
+        await _defferedAsync(_defferedTarget);
+}
