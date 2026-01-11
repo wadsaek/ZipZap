de778b62f934df03f03cf0ca138aab3d9787fe79
wadsaek <wadsaek@gmail.com>
2026-01-11 18:57:51 +0200

    make rsa a server host key algorithm

--- Sftp/Ssh/Algorithms/PublicKey/IPublicKeyAlgorithm.cs
+++ Sftp/Ssh/Algorithms/PublicKey/IPublicKeyAlgorithm.cs
@@ -1,13 +1,19 @@
+using System.Security.Cryptography;
+
 namespace ZipZap.Sftp.Ssh.Algorithms;
 
 public interface IPublicKeyAlgorithm : INamed {
     public byte[] Sign(byte[] unsigned, byte[] key);
 }
 
-public class RsaKeyAlgorithm : IPublicKeyAlgorithm {
+public class RsaKeyAlgorithm : IServerHostKeyAlgorithm {
     public NameList.Item Name => new NameList.GlobalName("rsa-sha2-512");
 
-    public byte[] Sign(byte[] unsigned, byte[] key) {
+    public HostKeyPair GetHostKeyPair() {
         throw new System.NotImplementedException();
+    }
+
+    public byte[] Sign(byte[] unsigned, byte[] key) {
+        var rsa = RSA.Create(new RSAParameters{});
     }
 }
