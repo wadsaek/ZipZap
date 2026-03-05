35c269b8ea614db24d61f95dbdf6c2c67a49f775
wadsaek <wadsaek@gmail.com>
2026-03-05 15:41:10 +0200

    FileService: rename FsoAccess repository files

--- Persistence/Repositories/FsoAccess/FsoAccessRepository.cs
+++ Persistence/Repositories/FsoAccess/FsoAccessRepository.cs
@@ -1,4 +1,4 @@
-﻿// FsosRepository.cs - Part of the ZipZap project for storing files online
+﻿// FsoAccessRepository.cs - Part of the ZipZap project for storing files online
 //     Copyright (C) 2026  Barenboim Esther wadsaek@gmail.com
 //
 //     This program is free software: you can redistribute it and/or modify
