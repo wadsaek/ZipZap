f1eb238820b67e1d7a62e2a0cb3291c93d7afdb7
wadsaek <wadsaek@gmail.com>
2026-03-21 15:14:26 +0200

    Front: extract user data into components

---
+++ Front/ViewComponents/UserSshKeys.cs
@@ -0,0 +1,31 @@
+// UserSshKeys.cs - Part of the ZipZap project for storing files online
+//     Copyright (C) 2026  Barenboim Esther wadsaek@gmail.com
+//
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+//
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY, without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+//
+//     You should have received a copy of the GNU General Public License
+//     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+
+using ZipZap.Classes;
+
+namespace ZipZap.Front.Components;
+
+[ViewComponent]
+public class UserSshKeys : ViewComponent {
+    public async Task<IViewComponentResult> InvokeAsync(IEnumerable<UserSshKey> keys) {
+        return View(keys);
+    }
+}
